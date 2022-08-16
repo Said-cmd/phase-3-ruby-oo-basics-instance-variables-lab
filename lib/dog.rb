0a76f741fc62208982e243ec9621a9d2ead3e1a2
Said Hussein <saidhussein12342@gmail.com>
2022-08-16 16:02:25 +0300

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,16 @@
 class Dog
-
+    # setter method
+    def name=(dog_name)
+        # assigns the @this_dogs_name instance variable a value of dog_name that it was passed as an argument
+        @this_dogs_name = dog_name
+    end
+    def name
+        @this_dogs_name
+    end
 end
+# creating the new dog
+lassie = Dog.new
+# call the Dog#name method with a value of "Lassie"
+lassie.name = "Lassie"
+# this is the same as calling lassie.name=("Lassie")
+puts lassie.name 
